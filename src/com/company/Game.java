5f707ad7dcd76da2956230784bfa7f651f912c6d
AndreElliot <andre_elliot@hotmail.com>
2021-01-13 15:34:25 +0100

    //Added some of the classes needed for the project.
    //added Constuctor in Class "player"
    
    //started working slightly on the "run" method in the "Game" Class

---
+++ src/com/company/Game.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Game {
+
+    int rounds;
+    int players;
+    Scanner scan = new Scanner(System.in);
+
+
+
+
+
+    public void run(){
+        System.out.println("Mata in antal spelare (1-4)");
+        int players = scan.nextInt();
+        for(int i = 0; i < players; i++){
+
+        }
+
+
+}
+
+
+
+}
---
+++ src/com/company/Player.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Player {
+
+    String name;
+    int money = 400;
+
+    public Player(String name){
+        this.name = name;
+    }
+}
